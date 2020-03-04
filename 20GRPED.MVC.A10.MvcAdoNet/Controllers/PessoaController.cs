ea6b5eba0e13331907ee1a2bf6b86e6af7a0fb36
Felipe Andrade <villela.felipe@gmail.com>
2020-03-04 08:16:01 -0300

    Add project files.

---
+++ 20GRPED.MVC.A10.MvcAdoNet/Controllers/PessoaController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _20GRPED.MVC.A10.MvcAdoNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _20GRPED.MVC.A10.MvcAdoNet.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PessoaModel newPessoaModel)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                _pessoaRepository.Add(newPessoaModel);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ 20GRPED.MVC.A10.MvcAdoNet/Models/PessoaModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _20GRPED.MVC.A10.MvcAdoNet.Models
+{
+    public class PessoaModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime Nascimento { get; set; }
+    }
+}
